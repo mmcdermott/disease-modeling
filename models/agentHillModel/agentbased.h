c43861dd7d23fc3575dd30f0a4e9df201603430c
Matt <mattmcdermott8@gmail.com>
2013-06-29 11:18:52 -0400

    adding cpp files

---
+++ models/agentHillModel/agentbased.h
@@ -0,0 +1,14 @@
+//
+//  agentbased.h
+//  
+//
+//  Created by mhcuser on 6/28/13.
+//
+//
+
+#ifndef ____agentbased__
+#define ____agentbased__
+
+#include <iostream>
+
+#endif /* defined(____agentbased__) */
