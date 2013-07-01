d1b9b52f6f62ca5735a65450945e2a9a9b34e9e7
ehartman4 <hartman_emma@wheatonma.edu>
2013-07-01 15:13:24 -0400

    adding turtle files

---
+++ models/agentHillModel/turtle.cpp
@@ -0,0 +1,9 @@
+//
+//  turtle.cpp
+//  
+//
+//  Created by mhcuser on 7/1/13.
+//
+//
+
+#include "turtle.h"
---
+++ models/agentHillModel/turtle.h
@@ -0,0 +1,14 @@
+//
+//  turtle.h
+//  
+//
+//  Created by mhcuser on 7/1/13.
+//
+//
+
+#ifndef ____turtle__
+#define ____turtle__
+
+#include <iostream>
+
+#endif /* defined(____turtle__) */
