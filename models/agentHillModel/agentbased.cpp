6e57be4fa6e90d846f8a7fac5e2c32b9f1182f9f
Matt <mattmcdermott8@gmail.com>
2013-07-01 14:46:33 -0400

    Starting splitting up into header files

--- models/agentHillModel/agentbased.cpp
+++ models/agentHillModel/agentbased.cpp
@@ -12,42 +12,6 @@ using namespace std;
 
 int main()
 {
-    double mu0;     //Natural mortality rate USB per year
-    double mu1;     // Natural mortality rate FB per year
-    double ro;      // USB birth rate per year
-    double alpha;   // FB birth rate per year
-    double p;       // Fraction of new infectionsn which are acute (fast progressors)
-    double vF;      // Progression rate of acute infection per year
-    double l0;      // Prevalence of LTBI in the USB population in 2000
-    double l1;      // Prevalence of LTBI in the FB population in 2000
-    double r0;      // Fraction of cases due to reactivation in the USB population
-    double r1;      // Fraction of cases due to reactivation in the FB population
-    double vL0;     // Progression rate for reactivation (chronic LTBI) in the USB population per year
-    double vL1;     // Progression rate for reactivation (chronic LTBI) in the FB population per year
-    double q;       // Fraction of infections progressing to infectious disease
-    double mud;     // Mortality rate due to TB per year
-    double x;       // Fraction of re-infected chronic LTBI moving to acute infection
-    double f;       // Fraction of FB arrivals with LTBI
-    double ARI0;    // Annual risk of infection for USB in 2000
-    double beta;    // Effective contact rate per year
-    double e0;      // Fraction of preferred contacts with own population for USB
-    double e1;      // Fraction of preferred contacts with own population for FB
-    double g;       // Fraction of FB arrivals with LTBI who are fast progressors
-    double phi0;    // Cumulative fraction self-cure and treatment of active disease for both populations per year RATES (USB)
-    double phi1;    // Cumulative fraction self-cure and treatment of active disease for both populations per year RATES (FB)
-    double sigmaF0; // Cumulative fraction of treatment for acute infection for both populations per year RATES (USB)
-    double sigmaF1; // Cumulative fraction of treatment for acute infection for both populations per year RATES (FB)
-    double sigmaL;  // Treatment rate for chronic LTBI per year
-         
-                // We handle susceptibles only by population
-    double S0pop;   // population of USB Susceptibles (S0)
-    double S1pop;   // population of FB Susceptibles (S1)
-                // "Snapshot" variables which need to be global
-    double deltaS0;
-    double deltaS1;
-    double totpop;
-    double lambda0;
-    double lambda1;
 
     turtles-own [
              int dstate        // state of TB disease (latentLTBI=0, acuteLTBI=1, infectiousATBI=2, non-infectiousATBI=3)
@@ -56,9 +20,9 @@ int main()
              ]
 
 
-    mu0 =1 / 78;
-    mu1 =1 / 53;
-    ro =0.018;
+    mu0 = 1/78;
+    mu1 = 1/53;
+    ro  =0.018;
     alpha =0.005;
     p =0.103;
     vF =1.5;
