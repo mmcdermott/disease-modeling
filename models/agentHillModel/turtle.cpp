a5298128891cf9daf74374035a29cbdf34ccc6e6
Colin Pawlowski <colinpawlowski78@gmail.com>
2013-07-01 16:07:16 -0400

    started turtle class definition

--- models/agentHillModel/turtle.cpp
+++ models/agentHillModel/turtle.cpp
@@ -9,8 +9,25 @@
 using namespace std;
 #include "turtle.h"
 
+enum State {
+    Acute LATENT = 0,
+    Chronic LATENT = 1;
+};
+
 int main()
 {
 	mu0 = 1/78;
     mu1 = 1/53;
 }
+
+class turtle{
+private:
+    char COB;
+    int treatmentTimeLeft;
+    int newCost;
+public:
+    turtle(char COB, State);
+    char updateState(double[] probVector,int size);
+    char updateCost();
+};
+
