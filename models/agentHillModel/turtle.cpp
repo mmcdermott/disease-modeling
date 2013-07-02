9f6097b6f5531b5fac22c7d430989edda7236b1d
U-Colin-PC2\Colin <Colin@Colin-PC2.(none)>
2013-07-02 14:18:53 -0400

    made the turtle.cpp program better

--- models/agentHillModel/turtle.cpp
+++ models/agentHillModel/turtle.cpp
@@ -8,11 +8,8 @@
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
-#include <ctime>
-//#include <chrono>
-#include <tr1/random>
+#include <random>
 #include <functional>
-//#include <array>
 using namespace std;
 #include "turtle.h"
 
@@ -25,24 +22,20 @@ enum State {
 	CHRONIC_LATENT = 1,
 	INFECTIOUS_TB = 2,
 	NONINFECTIOUS_TB = 3,
-	SUSCEPTIBLE = 4
-};
-enum DeathType {
-	STILL_ALIVE = 0,
-	NATURAL_DEATH = 1,
-	TB_DEATH = 2
+	SUSCEPTIBLE = 4,
+	TB_DEATH = 5,
+	NATURAL_DEATH = 6
 };
 
 const char* countryNames[] = {"USA", "Other"};
-const char* stateNames[] = {"Acute Latent (F)", "Chronic Latent (L)", "Infectious TB (I)", "Non-Infectious TB (J)", "Susceptible (S)", "Dead"};
-const char* deathTypeNames[] = {"Still Alive", "Natural Death", "TB Death"};
+const char* stateNames[] = {"Acute Latent (F)", "Chronic Latent (L)", "Infectious TB (I)",
+							"Non-Infectious TB (J)", "Susceptible (S)", "TB Death", "Natural Death"};
 
 //Declaration
 class turtle{
 private:
     COB country;
 	State state;
-    DeathType deathType;
 	int treatmentTimeLeft;
     int newCost;
 	double mu;  //natural mortality rate
@@ -54,7 +47,6 @@ public:
     void display();
     COB getCountry();
     State getState();
-    DeathType getDeathType();
     int getTreatmentTimeLeft();
     int getNewCost();
     int getTimeSinceInfection();
@@ -67,7 +59,6 @@ turtle::turtle(COB c, State s){
 	state = s;
 	treatmentTimeLeft = 0;
 	newCost = 0;
-	deathType = STILL_ALIVE;
 	x = 0;
 	if(country == USA) mu = MU0;
 	else mu = MU1;
@@ -76,15 +67,20 @@ turtle::turtle(COB c, State s){
 //Updates turtle state, treatmentTimeLeft, and newCost for each iteration
 int turtle::updateState(double probVector[]){
 	srand(time(NULL));
+	
+	//Initializations
 	double r; //random number from (0,1]
 	int startingIndex = country*24 + state*6;
 	double probArray[6];
-	int i, result = 0;
+	int i, result = 0;  //result = next state
+	
+	//Find turtle-specific discrete probability distribution
 	for(i=0; i<6; i++)
 		probArray[i] = probVector[startingIndex + i];
+	
+	//Sample a random state "result"
 	r = (double)rand()/RAND_MAX; //random number from (0,1]
-	//printf("%f\n", r);
-	i = 0;
+		i = 0;
 	while(r > 0){
 		r -= probArray[i];
 		if(r < 0) result = i;
@@ -95,11 +91,29 @@ int turtle::updateState(double probVector[]){
 	//Natural Death Rate
 	r = (double)rand()/RAND_MAX; //random number from (0,1]
 	if(r < mu){
-		deathType = NATURAL_DEATH;
-		return 5;
+		result = NATURAL_DEATH;
 	}
-	if(result <= 4) state = static_cast<State>(result);
-	else deathType = TB_DEATH;
+	
+	//Calculate new costs of treatments
+	if(treatmentTimeLeft > 0){
+		if(state == ACUTE_LATENT || state == CHRONIC_LATENT)
+			newCost += LATENT_TREATMENT_COST / LATENT_TREATMENT_LENGTH;
+		if(state == INFECTIOUS_TB || state == NONINFECTIOUS_TB)
+			newCost += ACTIVE_TREATMENT_COST / ACTIVE_TREATMENT_LENGTH;
+		
+		treatmentTimeLeft--;
+	}
+	else { //probability of entering treatment
+		r = (double)rand()/RAND_MAX; //random number from (0,1]
+		if(r < PROB_ACTIVE_TREATMENT){
+			if(state == INFECTIOUS_TB || state == NONINFECTIOUS_TB)
+				treatmentTimeLeft = ACTIVE_TREATMENT_LENGTH;
+			else if(r < PROB_LATENT_TREATMENT && (state == ACUTE_LATENT || state == CHRONIC_LATENT) )
+				treatmentTimeLeft = LATENT_TREATMENT_LENGTH;
+		}
+	}
+	
+	state = static_cast<State>(result);
 	return result;
 }
 
@@ -108,8 +122,7 @@ void turtle::display(){
 	cout << "Health State: " << stateNames[state] << "\n";
 	cout << "Treatment Time Left: " << treatmentTimeLeft << "\n";
 	cout << "New cost: $" << newCost << "\n";
-	cout << "Natural death rate: " << mu << "\n";
-	cout << "Natural death: " << deathTypeNames[deathType] << "\n\n";
+	cout << "Natural death rate: " << mu << "\n\n";
 }
 
 COB turtle::getCountry() {
@@ -117,9 +130,6 @@ COB turtle::getCountry() {
 }
 State turtle::getState() {
 	return state;
-}
-DeathType turtle::getDeathType() {
-	return deathType;
 }
 int turtle::getTreatmentTimeLeft() {
 	return treatmentTimeLeft;
@@ -145,6 +155,9 @@ int main()
 	for(i=0;i<48;i+=6)
 		probVector[i] = 0.0;
 		
+	t.updateState(probVector);
+	t.display();
+	
 	t.updateState(probVector);
 	t.display();
 }
--- models/agentHillModel/turtle.h
+++ models/agentHillModel/turtle.h
@@ -11,9 +11,18 @@
 
 #include <iostream>
 
+const double DELTA_T = 1;  //1 month
 
 const double MU0 = 1./78; //Natural mortality rate USB per year
 const double MU1 = 1./53; // Natural mortality rate FB per year
 
+const double LATENT_TREATMENT_COST = 500;  //9 months of isoniazid
+const double ACTIVE_TREATMENT_COST = 6000; //9 month medications + hospitalizations
+
+const double LATENT_TREATMENT_LENGTH = 9;  //9 months
+const double ACTIVE_TREATMENT_LENGTH = 9;  //9 months
+
+const double PROB_ACTIVE_TREATMENT = 1.;   //probability of someone with active TB starting treatment every timestep
+const double PROB_LATENT_TREATMENT = .5; //probability of someone with latent TB starting treatment every timestep
 
 #endif /* defined(____turtle__) */
