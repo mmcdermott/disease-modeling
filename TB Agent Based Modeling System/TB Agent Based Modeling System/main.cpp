3bb1cabb3ab3d9013fc12699af41536b54689952
Matt McDermott <mattmcdermott8@gmail.com>
2013-07-03 14:53:21 -0400

    fixed scope error by casting totT to an int rather than flooring it

---
+++ TB Agent Based Modeling System/TB Agent Based Modeling System/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TB Agent Based Modeling System
+//
+//  Created by REU students 2013 on 7/3/13.
+//  Copyright (c) 2013 REU students 2013. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
