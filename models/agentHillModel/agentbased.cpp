3d28f2d353544cf03ecef8e360db0ffb7b65f7f6
fhorngeek14 <rkimura@oberlin.edu>
2013-07-09 05:24:49 -0400

    reverted change to acute arrivals

--- models/agentHillModel/agentbased.cpp
+++ models/agentHillModel/agentbased.cpp
@@ -280,7 +280,7 @@ int main()
     int newf1           = floor(p * numFBInfections);
     int newl1           = numFBInfections - newf1;
     int LTBIArrivals    = floor(f * alpha * (N0[i-1] + N1[i-1]) * DELTA_T);
-    int AcuteArrivals   = floor(g * p * LTBIArrivals * DELTA_T);
+    int AcuteArrivals   = floor(g * p * LTBIArrivals);
     newf1              += AcuteArrivals;
     newl1              += LTBIArrivals - AcuteArrivals;
     S1[i]              -= numFBInfections;
