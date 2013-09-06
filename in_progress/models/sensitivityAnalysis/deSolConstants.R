44865b8ca0eeea31728e69f1db2d0f63683f0575
Colin <colinpawlowski78@gmail.com>
2013-09-06 16:40:27 -0400

    changed cutOffYear to 2000

--- in_progress/models/sensitivityAnalysis/deSolConstants.R
+++ in_progress/models/sensitivityAnalysis/deSolConstants.R
@@ -2,7 +2,7 @@
 deltaT    <- 0.8    #The length of each time step (years).
 initialYr <- 2000
 finalYr   <- 2100
-cutoffYr  <- 2008
+cutoffYr  <- 2000
 totT      <- floor((finalYr-initialYr)/deltaT)  #Time steps
 cutoffT   <- floor((cutoffYr-initialYr)/deltaT) + 1
 years     <- seq(initialYr+deltaT,finalYr,deltaT)
